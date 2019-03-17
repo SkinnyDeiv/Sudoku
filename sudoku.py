6b45c18a7b30c5e3a9d0c91350d31196b523930f
DeivOsrn <felipe.osrn@gmail.com>
2019-03-16 20:56:11 -0600

    Create my first threads

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,14 @@
 import threading
+
+class myThread(threading.Thread):
+
+    def __init__(self, i):
+        threading.Thread.__init__(self)
+        self.h = i
+
+    def run(self):
+        print("Value send", self.h)
+
+thread1 = myThread(1)
+thread1.start()
+
