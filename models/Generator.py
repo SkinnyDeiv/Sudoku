1108df718db77e7c282b6abf8b9b6ffeb1da2ab3
FelipeOsornio <felipe.osrn@gmail.com>
2020-07-13 21:19:59 -0500

    Updating folders

--- models/Generator.py
+++ models/Generator.py
@@ -1,47 +1,43 @@
 import numpy
 import random
 
-from Position import Position
 from Validator import Validator
 
 
 class Generator:
     SUDOKU_NUMBER = 9
+    GRID_SIZE = 81
 
     def __init__(self):
         self.grid = numpy.zeros((9, 9))
-        self.backtracking = []
+        self.route = []
         self.validator = Validator(self.grid)
 
-    def change_value(self, last_position):
-        last_element = self.grid[last_position.row][last_position.column]
-
-        if last_element not in last_position.no_candidates:
-            last_position.no_candidates.append(last_element)
-            self.grid[last_position.row][last_position.column] = random.randint(1, 9)
-
-            if len(last_position.no_candidates) > 8:
-                return
-
-    def generate_sudoku(self):
-        while len(self.backtracking) < 10:
+    def fill_places(self):
+        for i in range(10):
             while self.validator.validation():
-                position = Position(random.randrange(9), random.randrange(9))
-                self.backtracking.append(position)
-                self.grid[position.row][position.column] = random.randint(1, 9)
+                position = (random.randrange(9), random.randrange(9))
+                if position not in self.route:
+                    self.route.append(position)
+                    self.grid[position[0]][position[1]] = random.randint(1, 9)
 
-            self.change_value(self.backtracking[-1])
+            invalid_value = self.route.pop()
+            self.grid[invalid_value[0]][invalid_value[1]] = 0.0
 
-        return self.grid
-
-    def pretty_print(self):
-        for row in self.grid:
-            print(row)
+            last_position = self.route.pop()
+            for possible_value in range(9):
+                self.grid[last_position[0]][last_position[1]] = possible_value
+                if self.validator.validation():
+                    break
+                else:
+                    continue
 
 
 generator = Generator()
-sudoku = generator.generate_sudoku()
-print(sudoku)
+generator.fill_places()
 
-validator = Validator(sudoku)
-print(validator.validation())
+for row in generator.grid:
+    print(row)
+
+validator = Validator(generator.grid)
+print(validator.validation(), len(generator.route))
